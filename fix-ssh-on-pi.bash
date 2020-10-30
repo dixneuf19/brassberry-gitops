5a08b9b08bdfa80f9f3fb3a3aff0a00b861fafb4
Julen Dixneuf <julend@padok.fr>
2020-10-30 17:21:50 +0100

    feat: adapt script for my usage

--- fix-ssh-on-pi.bash
+++ fix-ssh-on-pi.bash
@@ -34,6 +34,10 @@ settings_file="fix-ssh-on-pi.ini"
 
 # You should not need to change anything beyond here.
 
+set -o errexit    # Used to exit upon error, avoiding cascading errors
+# set -o pipefail   # Unveils hidden failures
+set -o nounset    # Exposes unset variables
+
 if [ -e "${settings_file}" ]
 then
   source "${settings_file}"
@@ -44,68 +48,76 @@ elif [ -e "${0%.*}.ini" ]
 then
   source "${0%.*}.ini"
 else
-  echo "ERROR: Can't find the Settings file \"${settings_file}\""
+  echo "💣 ERROR: Can't find the Settings file \"${settings_file}\""
   exit 1
 fi
 
 variables=(
-  root_password_clear
-  pi_password_clear
-  public_key_file
-  wifi_file
+  github_username
+  static_ip
+  hostname
 )
 
 for variable in "${variables[@]}"
 do
   if [[ -z ${!variable+x} ]]; then   # indirect expansion here
-    echo "ERROR: The variable \"${variable}\" is missing from your \""${settings_file}"\" file.";
+    echo "💣 ERROR: The variable \"${variable}\" is missing from your \""${settings_file}"\" file.";
     exit 2
   fi
 done
 
-image_to_download="https://downloads.raspberrypi.org/raspios_full_armhf_latest"
-url_base="https://downloads.raspberrypi.org/raspios_full_armhf/images/"
-version="$( wget -q ${url_base} -O - | xmllint --html --xmlout --xpath 'string(/html/body/table/tr[last()-1]/td/a/@href)' - )"
-sha_file=$( wget -q ${url_base}/${version} -O - | xmllint --html --xmlout --xpath 'string(/html/body/table/tr/td/a[contains(@href, "256")])' - )
-sha_sum=$( wget -q "${url_base}/${version}/${sha_file}" -O - | awk '{print $1}' )
+
+image_to_download="https://cdimage.ubuntu.com/releases/20.04.1/release/ubuntu-20.04.1-preinstalled-server-arm64+raspi.img.xz"
+image_name="ubuntu-20.04.1-preinstalled-server-arm64+raspi.img.xz"
+sha_gpg_url="https://cdimage.ubuntu.com/releases/20.04.1/release/SHA256SUMS.gpg"
+sha_sum_url="https://cdimage.ubuntu.com/releases/20.04.1/release/SHA256SUMS"
+
+
 sdcard_mount="/mnt/sdcard"
 
 if [ $(id | grep 'uid=0(root)' | wc -l) -ne "1" ]
 then
-    echo "You are not root "
+    echo "💣 You are not root "
     exit
 fi
 
-if [ ! -e "${public_key_file}" ]
-then
-    echo "Can't find the public key file \"${public_key_file}\""
-    echo "You can create one using:"
-    echo "   ssh-keygen -t ed25519 -f ./${public_key_file} -C \"Raspberry Pi keys\""
-    exit 3
-fi
+# if [ ! -e "${public_key_file}" ]
+# then
+#     echo "Can't find the public key file \"${public_key_file}\""
+#     echo "You can create one using:"
+#     echo "   ssh-keygen -t ed25519 -f ./${public_key_file} -C \"Raspberry Pi keys\""
+#     exit 3
+# fi
 
 function umount_sdcard () {
-    umount "${sdcard_mount}"
+    umount "${sdcard_mount}" || echo "umount: \"${sdcard_mount}\": not mounted."
     if [ $( ls -al "${sdcard_mount}" | wc -l ) -eq "3" ]
     then
-        echo "Sucessfully unmounted \"${sdcard_mount}\""
+        echo "✅ Sucessfully unmounted \"${sdcard_mount}\""
         sync
     else
-        echo "Could not unmount \"${sdcard_mount}\""
+        echo "💣 Could not unmount \"${sdcard_mount}\""
         exit 4
     fi
 }
 
 # Download the latest image, using the  --continue "Continue getting a partially-downloaded file"
-wget --continue ${image_to_download} -O raspbian_image.zip
+echo "⚙ Downloading the image"
+wget --continue ${image_to_download} -O ${image_name}
+echo "✅ Downloaded the image"
 
-echo "Checking the SHA-1 of the downloaded image matches \"${sha_sum}\""
 
-if [ $( sha256sum raspbian_image.zip | grep ${sha_sum} | wc -l ) -eq "1" ]
+echo "⚙ Checking the SHA256 of the downloaded image matches"
+
+curl -sS -o SHA256SUMS.gpg $sha_gpg_url
+curl -sS -o SHA256SUMS $sha_sum_url
+gpg --keyid-format long --verify SHA256SUMS.gpg SHA256SUMS || echo "OK"
+
+if [ $( sha256sum -c SHA256SUMS 2>&1 | grep OK | wc -l ) -eq "1" ]
 then
-    echo "The sha_sums match"
+    echo "✅ The sha_sums match"
 else
-    echo "The sha_sums did not match"
+    echo "💣 The sha_sums did not match"
     exit 5
 fi
 
@@ -114,107 +126,112 @@ then
   mkdir ${sdcard_mount}
 fi
 
-# unzip
-extracted_image=$( 7z l raspbian_image.zip | awk '/-raspios-/ {print $NF}' )
-echo "The name of the image is \"${extracted_image}\""
+extracted_image=$(echo ${image_name} | sed 's/.xz//')
+if [ -e ${extracted_image} ]
+then
+    echo "⚙ Deleting existing extracted image for idempotence"
+    rm -f ${extracted_image}
+fi
 
-7z x -y raspbian_image.zip
+echo "⚙ Extracting \"${image_name}\" to \"${extracted_image}\""
+xz --decompress ${image_name} --keep
 
 if [ ! -e ${extracted_image} ]
 then
-    echo "Can't find the image \"${extracted_image}\""
+    echo "💣 Can't find the image \"${extracted_image}\""
     exit 6
 fi
 
 umount_sdcard
-echo "Mounting the sdcard boot disk"
+echo "⚙ Mounting the sdcard boot disk"
 
 loop_base=$( losetup --partscan --find --show "${extracted_image}" )
 
-echo "Running: mount ${loop_base}p1 \"${sdcard_mount}\" "
+echo "⚙ Running: mount ${loop_base}p1 \"${sdcard_mount}\" "
 mount ${loop_base}p1 "${sdcard_mount}"
 ls -al /mnt/sdcard
-if [ ! -e "${sdcard_mount}/kernel.img" ]
+if [ ! -e "${sdcard_mount}/initrd.img" ]
 then
-    echo "Can't find the mounted card\"${sdcard_mount}/kernel.img\""
+    echo "💣 Can't find the mounted card\"${sdcard_mount}/initrd.img\""
     exit 7
 fi
+echo "✅ Mounted the sdcard boot disk"
 
-cp -v "${wifi_file}" "${sdcard_mount}/wpa_supplicant.conf"
-if [ ! -e "${sdcard_mount}/wpa_supplicant.conf" ]
-then
-    echo "Can't find the ssh file \"${sdcard_mount}/wpa_supplicant.conf\""
-    exit 8
-fi
 
+echo "⚙ Activating ssh"
 touch "${sdcard_mount}/ssh"
 if [ ! -e "${sdcard_mount}/ssh" ]
 then
-    echo "Can't find the ssh file \"${sdcard_mount}/ssh\""
+    echo "💣 Can't find the ssh file \"${sdcard_mount}/ssh\""
     exit 9
 fi
+echo "✅ SSH activated"
 
-if [ -e "${first_boot}" ]
+echo "Configure cloud init user-data"
+user_data_template_file="templates/user-data"
+if [ ! -e "${user_data_template_file}" ]
 then
-  cp -v "${first_boot}" "${sdcard_mount}/firstboot.sh"
-else
-  echo '#!/bin/bash' > "${sdcard_mount}/firstboot.sh"
-  echo "sed \"s/raspberrypi/\$( sed 's/://g' /sys/class/net/eth0/address )/g\" -i /etc/hostname /etc/hosts" >> "${sdcard_mount}/firstboot.sh"
-  echo '/sbin/shutdown -r 5 "reboot in five minutes"' >> "${sdcard_mount}/firstboot.sh"
+    echo "💣 Please provide a template file for user-data"
+    exit 9
 fi
+echo "⚙ Add SSH keys from GitHub user \"${github_username}\" and configure hostname to \"${hostname}\""
+GITHUB_USERNAME=${github_username} HOSTNAME=${hostname} envsubst < ${user_data_template_file} > "${sdcard_mount}/user-data"
+echo "✅ Cloud init configured for Github \"${github_username}\""
+
+
+network_config_template_file="templates/network-config"
+echo "⚙ Setting static ip: \"${static_ip}\", using ${network_config_template_file}"
+
+if [ ! -e "${network_config_template_file}" ]
+then
+    echo "💣 Please provide a template file for network-config"
+    exit 9
+fi
+STATIC_IP=${static_ip} envsubst < ${network_config_template_file} > "${sdcard_mount}/network-config"
+echo "✅ Set static IP to \"${static_ip}\" with cloud-init"
+
+echo "⚙ Enable cgroup limits support"
+sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' "${sdcard_mount}/cmdline.txt"
+echo "✅ cgroup limits support enabled"
+
+echo "⚙ Deactivate wifi and bluetooth"
+echo 'dtoverlay=disable-wifi' | tee -a "${sdcard_mount}/usercfg.txt"
+echo 'dtoverlay=disable-bt' | tee -a "${sdcard_mount}/usercfg.txt"
+echo "✅ cgroup limits support enabled"
 
 umount_sdcard
 
-echo "Mounting the sdcard root disk"
-echo "Running: mount ${loop_base}p2 \"${sdcard_mount}\" "
-mount ${loop_base}p2 "${sdcard_mount}"
-ls -al /mnt/sdcard
+# echo "⚙ Mounting the sdcard root disk"
+# echo "Running: mount ${loop_base}p2 \"${sdcard_mount}\" "
+# mount ${loop_base}p2 "${sdcard_mount}"
+# ls -al /mnt/sdcard
+# if [ ! -e "${sdcard_mount}/etc/shadow" ]
+# then
+#     echo "💣 Can't find the mounted card\"${sdcard_mount}/etc/shadow\""
+#     exit 10
+# fi
+# echo "✅ Mounted the sdcard root disk"
 
-if [ ! -e "${sdcard_mount}/etc/shadow" ]
-then
-    echo "Can't find the mounted card\"${sdcard_mount}/etc/shadow\""
-    exit 10
-fi
+# echo "⚙ Change sshd_config file"
+# sed -e 's;^#PasswordAuthentication.*$;PasswordAuthentication no;g' -e 's;^PermitRootLogin .*$;PermitRootLogin no;g' -i "${sdcard_mount}/etc/ssh/sshd_config"
+# mkdir "${sdcard_mount}/home/ubuntu/.ssh"
+# chmod 0700 "${sdcard_mount}/home/ubuntu/.ssh"
+# chown 1000:1000 "${sdcard_mount}/home/ubuntu/.ssh"
+# curl ${public_key_url} -o "${sdcard_mount}/home/ubuntu/.ssh/authorized_keys"
+# chown 1000:1000 "${sdcard_mount}/home/ubuntu/.ssh/authorized_keys"
+# chmod 0600 "${sdcard_mount}/home/ubuntu/.ssh/authorized_keys"
+# echo "✅ SSH configured"
 
-echo "Change the passwords and sshd_config file"
+# echo "⚙ Configure hostname to \"${hostname}\""
+# echo ${hostname} > "${sdcard_mount}/etc/hostname"
+# echo "✅ Hostname configured"
 
-root_password="$( python3 -c "import crypt; print(crypt.crypt('${root_password_clear}', crypt.mksalt(crypt.METHOD_SHA512)))" )"
-pi_password="$( python3 -c "import crypt; print(crypt.crypt('${pi_password_clear}', crypt.mksalt(crypt.METHOD_SHA512)))" )"
-sed -e "s#^root:[^:]\+:#root:${root_password}:#" "${sdcard_mount}/etc/shadow" -e  "s#^pi:[^:]\+:#pi:${pi_password}:#" -i "${sdcard_mount}/etc/shadow"
-sed -e 's;^#PasswordAuthentication.*$;PasswordAuthentication no;g' -e 's;^PermitRootLogin .*$;PermitRootLogin no;g' -i "${sdcard_mount}/etc/ssh/sshd_config"
-mkdir "${sdcard_mount}/home/pi/.ssh"
-chmod 0700 "${sdcard_mount}/home/pi/.ssh"
-chown 1000:1000 "${sdcard_mount}/home/pi/.ssh"
-cat ${public_key_file} >> "${sdcard_mount}/home/pi/.ssh/authorized_keys"
-chown 1000:1000 "${sdcard_mount}/home/pi/.ssh/authorized_keys"
-chmod 0600 "${sdcard_mount}/home/pi/.ssh/authorized_keys"
+# umount_sdcard
 
-echo "[Unit]
-Description=FirstBoot
-After=network.target
-Before=rc-local.service
-ConditionFileNotEmpty=/boot/firstboot.sh
-
-[Service]
-ExecStart=/boot/firstboot.sh
-ExecStartPost=/bin/mv /boot/firstboot.sh /boot/firstboot.sh.done
-Type=oneshot
-RemainAfterExit=no
-
-[Install]
-WantedBy=multi-user.target" > "${sdcard_mount}/lib/systemd/system/firstboot.service"
-
-cd "${sdcard_mount}/etc/systemd/system/multi-user.target.wants" && ln -s "/lib/systemd/system/firstboot.service" "./firstboot.service"
-cd -
-
-umount_sdcard
-
-new_name="${extracted_image%.*}-ssh-enabled.img"
+new_name="${extracted_image%.*}-${hostname}.img"
 cp -v "${extracted_image}" "${new_name}"
 
 losetup --detach ${loop_base}
-
-lsblk
 
 echo ""
 echo "Now you can burn the disk using something like:"
