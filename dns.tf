7a8ac1c1e4e059b320914bcbcf948725c016a1ba
dixneuf19 <julen.dixneuf@gmail.com>
2024-10-10 16:49:36 +0200

    fix: deploy DNS for the new domain

--- dns.tf
+++ dns.tf
@@ -3,5 +3,13 @@ resource "gandi_livedns_record" "oracle_arm" {
   ttl    = 300
   type   = "A"
   values = [oci_core_instance.oracle-arm.public_ip]
+  zone   = "dixneuf19.fr"
+}
+
+resource "gandi_livedns_record" "old_redirection" {
+  name   = "brassberry"
+  ttl    = 300
+  type   = "A"
+  values = [oci_core_instance.oracle-arm.public_ip]
   zone   = "dixneuf19.me"
 }
